5565612fde087485357ffe2450491f815266ec9a
Hou-Rui <13244639785@163.com>
2019-08-28 00:25:33 +0800

    Add spin.rkt

---
+++ spin.rkt
@@ -0,0 +1,10 @@
+#lang racket/gui
+
+(require "rune.rkt")
+
+(define (spin p a x n)
+  (letrec ([na (+ a (* x (/ pi n)))])
+    (if (= x n) (random-color p)
+        (above (random-color p) (spin (rotate na p) na (+ x 1) n)))))
+
+(show (spin ribbon 0 0 50))
